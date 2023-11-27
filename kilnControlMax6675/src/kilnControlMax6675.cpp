acde692ad42aec3081e4c74068931d508c5ef7a7
R S <rebeccasnyder505@gmail.com>
2023-11-27 16:28:13 -0700

    starting

---
+++ kilnControlMax6675/src/kilnControlMax6675.cpp
@@ -0,0 +1,51 @@
+/* 
+ * Project: Kiln Controller via MAX6675 K-Thermocouple Converter (0°C to +1024°C)
+ * Author: Rebecca Snyder
+ * Date: 2023-11-27
+ * 
+ * 
+ * 
+ * must install library "Adafruit_SSD1306" with ctrl-shift-p
+ * 
+ */
+
+
+#include "Particle.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+//MAX 6675 K-thermocouple converter
+const int SCKPIN = D17;
+const int CSPIN = D15;
+const int SOPIN = D16;
+
+int *thermoArray[11];
+
+int readTemperatureThermocouple(*thermoArray);
+
+
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  readTemperatureThermocouple(thermoArray);
+}
+
+int readTemperatureThermocouple(*thermoArray) {
+  int i = 0;
+  for (i=0;i<=11;i++) {
+    thermoArray[i] = digitalread(SOPIN);
+    Serial.printf("place %d is value %d \n",i,thermoArray[i]);
+  }
+}
+
+
+
