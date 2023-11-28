ca5a0e92d50ab3a0de9a3538e13a307fdc1a54e7
R S <rebeccasnyder505@gmail.com>
2023-11-27 17:37:23 -0700

    getting zeros

--- kilnControlMax6675/src/kilnControlMax6675.cpp
+++ kilnControlMax6675/src/kilnControlMax6675.cpp
@@ -6,22 +6,24 @@
  * 
  * 
  * must install library "Adafruit_SSD1306" with ctrl-shift-p
- * 
+ * must install libary "IoTClassroom_CNM" with ctrl-shift-p
  */
 
 
 #include "Particle.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
+#include "Button.h"
 
 //MAX 6675 K-thermocouple converter
 const int SCKPIN = D17;
 const int CSPIN = D15;
 const int SOPIN = D16;
+const int buttonPin = D6;
 
-int *thermoArray[11];
+int thermoArray[12];
 
-int readTemperatureThermocouple(*thermoArray);
+//int readTemperatureThermocouple (*thermoArray);
 
 
 
@@ -32,20 +34,34 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(SCKPIN, INPUT);
+  pinMode(CSPIN, INPUT);
+  pinMode(SOPIN, INPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  readTemperatureThermocouple(thermoArray);
-}
-
-int readTemperatureThermocouple(*thermoArray) {
+//  readTemperatureThermocouple(*thermoArray);
   int i = 0;
   for (i=0;i<=11;i++) {
-    thermoArray[i] = digitalread(SOPIN);
-    Serial.printf("place %d is value %d \n",i,thermoArray[i]);
+    Serial.printf("%d %d %d \n", digitalRead(SCKPIN), digitalRead(CSPIN), digitalRead(SOPIN));
+
+//   thermoArray[i] = digitalRead(SOPIN);
+//    Serial.printf("place %d is value %d \n",i,thermoArray[i]);
+    delay(100);
   }
 }
+
+// int readTemperatureThermocouple(*thermoArray) {
+//   int i = 0;
+//   for (i=0;i<=11;i++) {
+//     thermoArray[i] = digitalread(SOPIN);
+//     Serial.printf("place %d is value %d \n",i,thermoArray[i]);
+//   }
+// }
+
+
 
 
 
