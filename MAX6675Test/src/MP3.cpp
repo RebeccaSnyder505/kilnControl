a05321299e8487f923763b0dbeee775a1ca928da
R S <rebeccasnyder505@gmail.com>
2023-11-28 17:01:07 -0700

    MAX6675Test.cpp gives nonzerovalues, no change yet with lighter, but promising

---
+++ MAX6675Test/src/MP3.cpp
@@ -0,0 +1,9 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
