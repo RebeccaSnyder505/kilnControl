f217389c52f25c82d1a4014feac19646f6d38139
R S <rebeccasnyder505@gmail.com>
2023-12-01 17:04:03 -0700

    currently working on relay control in response to thermocouple input

--- kilnControlMain/src/kilnControlMain.cpp
+++ kilnControlMain/src/kilnControlMain.cpp
@@ -19,6 +19,8 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+#define AFAP 50000 // "As Fast As Possible" -- use more degrees than is possible
+
 //thermocouple
 MAX6675 thermocouple;
 byte thermocoupleStatus;
@@ -28,10 +30,41 @@ float tempC, tempF;
 #define OLED_RESET D4
 Adafruit_SSD1306 display(OLED_RESET);
 
+const int numberOfSegments = 5; // conventionally numbering starts at 1
+// glass full fuse sample firing schedule, source: https://www.bullseyeglass.com/wp-content/uploads/2023/02/technotes_04.pdf
+//    for two layers of 3mm (1/8") thickness, 12" diam with top and side elements
+int firingSchedule1 [numberOfSegments] [3] = {
+  // DPH (degrees F per hour), target temp in F, hold time in minutes
+  {400, 1250, 30}, // initial heat and rapid soak
+  {600, 1490, 10}, // rapid heat process soak
+  {AFAP, 900, 30}, // rapid cool anneal soak
+  {150, 700, 0},  // anneal cool
+  {AFAP, 70, 0}
+};
+
+int currentSegment;
+int TargetTemp;
+
+//kiln temperature related
+int kilnFarenheitEndDataActual [numberOfSegments]; //will want the temp of ending time
+int kilnTimeStartDataActual [numberOfSegments]; //will want the "start" of ending time
+
+int currentTempKiln; // current temp in fahrenheit
+int timeSegmentStarted; 
+float tempDifferenceFromSegmentStart;
+double targetSlope;
+double currentSlope;
+
+// note, will need to account for this somehow
+//target temperature, "reached" should be temp remaining in range
+
+
+
+
 
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
-
+void displayTemperatures(float fahrenheit, float celsius);
 
 
 
@@ -52,6 +85,7 @@ void setup() {
   //OLED setup
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // display I am using has I2C addr 0x3C (for the 128x64)
 
+
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -66,20 +100,23 @@ void loop() {
   Serial.printf("raw data %d \n", thermocouple.getRawData());
   tempC = thermocouple.getTemperature();
   tempF = (9.0/5.0)* tempC + 32;
-  Serial.printf("Temperature:%0.2f or %0.2f\n",tempC,tempF);
+  Serial.printf("Temperature:%0.2f or %0.2f\n",tempF,tempC);
   
+  //OLED
+  displayTemperatures(tempF,tempC);
+  delay(2000);
 
-   //OLED
+
+}
+
+void displayTemperatures(float fahrenheit, float celsius) {
   display.clearDisplay();   // clears the screen and buffer
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.printf("F %f \n",tempF);
+  display.printf("F %f \n",fahrenheit);
   display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.printf("C %f \n",tempC);  display.setTextColor(WHITE);
+  display.printf("C %f \n",celsius); 
   display.display();
-  delay(2000);
 }
- 
-
 
